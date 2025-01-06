e866faf82e3fa81a9a123778971952fa8aede263
Brick Ellis <brickellis@yahoo.com>
2025-01-06 14:16:43 -0700

    Now it is good

--- src/lib/components/App.svelte
+++ src/lib/components/App.svelte
@@ -1,35 +1,17 @@
 <script>
-  import { onMount } from 'svelte';
-  import { Canvas } from '@threlte/core';
-  import Scene from './Scene.svelte';
-  import Initialize from './Initialize.svelte';
-  let score = 0;
-  let sceneInitialized = false; 
+  import { Canvas } from '@threlte/core'
+  import Scene from './Scene.svelte'
+  let score = 0; 
 
   function handleScoreUpdate(event) {
-    score = event.detail;
+    score = event.detail; 
   }
-
-  onMount(async () => {
-    await new Promise(resolve => {
-      sceneInitialized = false; 
-      const unsubscribe = Initialize.subscribe(() => {
-        sceneInitialized = true;
-        unsubscribe(); 
-        resolve(); 
-      });
-    });
-  });
 </script>
 
-{#if sceneInitialized} 
-  <div style="position: absolute; top: 0.5em; left: 0.5em; color: white; z-index: 1; font-size: 2em;">
-    Score: {score}
-  </div>
+<div style="position: absolute; top: 1em; left: 1em; color: white; z-index: 1; font-size: 2em;"> 
+  Score: {score}
+</div>
 
-  <Canvas>
-    <Scene on:score={handleScoreUpdate} /> 
-  </Canvas>
-{/if}
-
-<Initialize />
+<Canvas>
+  <Scene on:score={handleScoreUpdate} /> 
+</Canvas>
