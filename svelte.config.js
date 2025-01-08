638a32e7e8c77cc6c48cc11bf8466ba0cec4d23a
Brick Ellis <brickellis@yahoo.com>
2025-01-08 10:37:24 -0700

    Try again 2

--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,15 @@
 import adapter from '@sveltejs/adapter-static';
 
-export default {
-  kit: {
-    adapter: adapter(),
-    prerender: {
-      entries: ['*'],
-    },
-    paths: {
-      base: '/flappy-bell',
-    },
-  },
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	kit: {
+		adapter: adapter({
+			fallback: '404.html'
+		}),
+		paths: {
+			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+		}
+	}
 };
+
+export default config;
