60c6549a25b4f6433877226ec916ae7222fd47e2
Brick Ellis <brickellis@yahoo.com>
2025-01-08 09:33:49 -0700

    Checkpoint

---
+++ test.py
@@ -0,0 +1,21 @@
+<script>
+  import { Canvas } from '@threlte/core'
+  import Scene from './Scene.svelte'
+  import ML from './ML.svelte'
+  // let score = 0; 
+
+  // function handleScoreUpdate(event) {
+  //   score = event.detail; 
+  // }
+</script>
+
+<!-- <div style="position: absolute; top: 1em; left: 1em; color: white; z-index: 1; font-size: 2em;"> 
+  Score: {score}
+</div> -->
+
+<ML /> 
+
+<!-- <Canvas>
+  <Scene on:score={handleScoreUpdate} /> 
+</Canvas> -->
+
