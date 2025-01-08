dcbed3d6c4339b24fc87df4e976d9f20464e50ee
Brick Ellis <brickellis@yahoo.com>
2025-01-08 10:48:55 -0700

    Try again 3

--- docs/_app/immutable/entry/start.CSP84ZWc.js
+++
@@ -1,1 +0,0 @@
-import{a as t}from"../chunks/entry.ReMSd-PZ.js";export{t as start};
--- svelte.config.js
+++ svelte.config.js
@@ -3,9 +3,6 @@ import adapter from '@sveltejs/adapter-static';
 export default {
   kit: {
     adapter: adapter(),
-    prerender: {
-      entries: ['*'],
-    },
     paths: {
       base: '/flappy-bell/docs',
     },
